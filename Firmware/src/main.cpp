08b261e8721f0a116f947aee333bbc67af2924ff
Daniel Grießhaber <dangrie158@gmail.com>
2018-12-29 07:42:01 +0100

    fixed display pin mapping and test function

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -21,11 +21,16 @@
 // enable debug messages to the serial port
 #define DEBUG
 
+// number of HV shift registers on the serial data bus
 #define NUM_CHIPS 6
+
+// number of digit tubes in the clock
+#define NUM_DIGITS 4
+
 // convert a chip and pin number to the corresponding bit
 // in the serial output stream of the shift registers
 // data is stored in host order (native)
-#define P2B(CHIP, PIN) (((CHIP) * 8) + (PIN))
+#define P2B(CHIP, PIN) ((((CHIP)-1) * 8) + (PIN))
 
 typedef struct
 {
@@ -56,17 +61,17 @@ const uint8_t clockPin = 4;
 const uint8_t dataPin = 0;
 
 // the mapping of [digit][number to display] to bitnumber in the serial output stream
-const uint8_t digitsPinmap[4][10]{
-    {P2B(1, 5), P2B(1, 7), P2B(1, 2), P2B(1, 1), P2B(1, 0), P2B(1, 7), P2B(1, 6), P2B(2, 6), P2B(2, 5), P2B(2, 4)},
+const uint8_t digitsPinmap[NUM_DIGITS][10]{
+    {P2B(1, 5), P2B(1, 3), P2B(1, 2), P2B(1, 1), P2B(1, 0), P2B(1, 7), P2B(1, 6), P2B(2, 6), P2B(2, 5), P2B(2, 4)},
     {P2B(3, 4), P2B(2, 3), P2B(2, 2), P2B(2, 1), P2B(2, 7), P2B(2, 0), P2B(3, 0), P2B(3, 1), P2B(3, 2), P2B(3, 3)},
     {P2B(5, 7), P2B(4, 4), P2B(4, 3), P2B(4, 5), P2B(4, 2), P2B(5, 2), P2B(4, 1), P2B(4, 7), P2B(4, 0), P2B(5, 1)},
     {P2B(6, 0), P2B(5, 4), P2B(5, 3), P2B(5, 5), P2B(5, 6), P2B(5, 2), P2B(6, 5), P2B(6, 3), P2B(6, 2), P2B(6, 1)}};
 
 // mapping of [led number] to bit in the serial output stream
-const uint8_t ledPinmap[2] = {P2B(3, 6), P2B(3, 7)};
+const uint8_t ledsPinmap[2] = {P2B(3, 6), P2B(3, 7)};
 
 // mapping of [number digit dot] to bit in the serial output stream
-const uint8_t dotPinmap[4] = {P2B(1, 4), P2B(3, 5), P2B(5, 0), P2B(6, 4)};
+const uint8_t dotsPinmap[NUM_DIGITS] = {P2B(1, 4), P2B(3, 5), P2B(5, 0), P2B(6, 4)};
 
 const String timezoneName = "Europe/Berlin";
 // API Key is defined in include/config.h
@@ -179,7 +184,7 @@ void updateTimeZoneInfo()
  * 
  * @param time time to display 
  */
-void display(tmElements_t time)
+void display(tmElements_t time, uint8_t dots = 0x00)
 {
   uint64_t serialStream = 0;
 
@@ -188,12 +193,28 @@ void display(tmElements_t time)
   serialStream |= 1ull << digitsPinmap[2][time.Minute / 10];
   serialStream |= 1ull << digitsPinmap[3][time.Minute % 10];
 
+  // set the outputs for the passed dot-state
+  for (uint8_t dot = 0; dot < NUM_DIGITS; dot++)
+  {
+    if ((dots >> dot) & 0b1)
+    {
+      serialStream |= 1ull << dotsPinmap[dot];
+    }
+  }
+
   // blink the LED seperator every other second
   if (time.Second % 2)
   {
-    serialStream |= (1ull << ledPinmap[0]);
-    serialStream |= (1ull << ledPinmap[1]);
+    serialStream |= (1ull << ledsPinmap[0]);
+    serialStream |= (1ull << ledsPinmap[1]);
   }
+
+#ifdef DEBUG
+  Serial.print("Displaying ");
+  Serial.print(time.Hour);
+  Serial.print(time.Second % 2 ? ":" : " ");
+  Serial.println(String(time.Minute));
+#endif
 
   // set latch pin low to avoid displaying the shifting of data
   digitalWrite(latchPin, LOW);
@@ -218,21 +239,21 @@ void display(tmElements_t time)
 
 void displayTest()
 {
-  // count up, colon off
+  // count up, colon off, dots alternate
   for (int8_t i = 0; i < 10; i++)
   {
     uint8_t doubleDigit = (i * 10) + i;
     tmElements_t testDisplay = {0, doubleDigit, doubleDigit};
-    display(testDisplay);
+    display(testDisplay, i % 2 ? 0b1111 : 0b0000);
     delay(300);
   }
 
-  // count down, colon on
+  // count down, colon on, dots alternate
   for (int8_t i = 9; i >= 0; i--)
   {
     uint8_t doubleDigit = (i * 10) + i;
     tmElements_t testDisplay = {1, doubleDigit, doubleDigit};
-    display(testDisplay);
+    display(testDisplay, i % 2 ? 0b1111 : 0b0000);
     delay(300);
   }
 }
